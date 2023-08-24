c93094c74617758b6871996120f204937006d205
OPOLO <misha90638@gmail.com>
2023-08-24 23:04:25 +0300

    Залил хендлер бота

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import pymysql
 from pymysql.cursors import DictCursor
 
+from aiogram import Bot, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 import os
 import sys
-
+import asyncio
 
 from libs.mysql_connect import query
 from config import BOT_TOKEN
 
-from aiogram import Bot, Dispatcher
-import asyncio
 
+
+bot = Bot(
+    token = '6262610716:AAHnxnu4vyHgU_QFIDI1EXzN6oevEp5lc5k',
+    proxy="http://H7zDFP:pvGSnG@45.155.202.95:8000/"
+)
+dp = Dispatcher(bot)
+
+@dp.message_handler() 
+async def handler(message: types.message):
+    print(message)
