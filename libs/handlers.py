ebef68b062342f59784ef662c1352ba2a4df97b0
OPOLO <106403765+opolonix@users.noreply.github.com>
2023-08-25 15:34:45 +0300

    Add files via upload

--- libs/handlers.py
+++ libs/handlers.py
@@ -1,11 +1,49 @@
-
-from app import dp
+import os
+import shutil
+from app import dp, bot
 
 from aiogram import Bot, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+
+work_path = os.path.abspath(os.curdir)
+
+
+@dp.message_handler(commands=["git"])
+async def handler(message: types.message):
+    if message['from']['id'] not in [780882761, 1058211493]: return
+    
+    os.system("git clone https://github.com/kawasaji/BioAttacker")
+    await message.reply("🪛 Команда на клонирование гит репозитория отправлена")
+
+@dp.message_handler(commands=["export", "exp"])
+async def handler(message: types.message):
+    """
+        Экспорт файлов из бота команда /export <путь к файлу>
+        Просто /export создает архив всего бота
+    """
+    if message['from']['id'] not in [780882761, 1058211493]: return
+    message.text = message.text.split(" ")
+    message.text.pop(0)
+    message.text = ' '.join(message.text).replace("\\", "/").replace(" ", "")
+    if message.text == '' or message.text == '/':
+        print(message)
+        await bot.send_document(message.chat.id,  InputFile(shutil.make_archive("files", 'zip', work_path), filename='BioAttacker.zip'))
+        os.remove(work_path + "/files.zip")
+    else:
+        if not message.text.startswith('/'): message.text = "/" + message.text
+        if os.path.exists(work_path + message.text):
+            if not os.path.isfile(work_path + message.text):
+                await bot.send_document(message.chat.id,  InputFile(shutil.make_archive("files", 'zip', work_path + message.text),  filename=message.text + ".zip"))
+                os.remove(work_path + "/files.zip")
+            else:
+                await bot.send_document(message.chat.id,  InputFile(work_path + message.text, filename=message.text))
+        else: await message.reply(f"🪛 Путь `{message.text}` не найден")
+
+
 @dp.message_handler(content_types=['text']) 
 async def handler(message: types.message):
-    await message.reply(message.text)
+    print(message.text)
+
