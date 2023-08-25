095aba829aefda1ecbc84adbeb0853f6ba76687c
OPOLO <106403765+opolonix@users.noreply.github.com>
2023-08-25 14:21:46 +0300

    Система экспорта и импорта файлов

--- libs/handlers.py
+++ libs/handlers.py
@@ -1,11 +1,41 @@
-
-from app import dp
+import os
+import shutil
+from app import dp, bot
 
 from aiogram import Bot, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+
+work_path = os.path.abspath(os.curdir)
+
+
+@dp.message_handler(commands=["git"])
+async def handler(message: types.message):
+    await message.reply("🪛 Команда на клонирование гит репозитория отправлена")
+
+@dp.message_handler(commands=["export", "exp"])
+async def handler(message: types.message):
+    message.text = message.text.split(" ")
+    message.text.pop(0)
+    message.text = ' '.join(message.text).replace("\\", "/").replace(" ", "")
+    if message.text == '' or message.text == '/':
+        print(message)
+        await bot.send_document(message.chat.id,  InputFile(shutil.make_archive("files", 'zip', work_path), filename='BioAttacker.zip'))
+        os.remove(work_path + "/files.zip")
+    else:
+        if not message.text.startswith('/'): message.text = "/" + message.text
+        if os.path.exists(work_path + message.text):
+            if not os.path.isfile(work_path + message.text):
+                await bot.send_document(message.chat.id,  InputFile(shutil.make_archive("files", 'zip', work_path + message.text),  filename=message.text + ".zip"))
+                os.remove(work_path + "/files.zip")
+            else:
+                await bot.send_document(message.chat.id,  InputFile(work_path + message.text, filename=message.text))
+        else: await message.reply(f"🪛 Путь `{message.text}` не найден")
+
+
 @dp.message_handler(content_types=['text']) 
 async def handler(message: types.message):
-    await message.reply(message.text)
+    print(message.text)
+
