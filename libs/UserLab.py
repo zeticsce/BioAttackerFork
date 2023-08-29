a6542c667e80aa1b4142ff3434c00f2e4e0b31d8
OPOLO <106403765+opolonix@users.noreply.github.com>
2023-08-29 14:49:49 +0300

    Update UserLab.py

--- libs/UserLab.py
+++ libs/UserLab.py
@@ -22,14 +22,14 @@ class UserLab:
         """
         
         result = query(f"SELECT * FROM `bio_attacker`.`labs` LEFT JOIN `telegram_data`.`tg_users` ON bio_attacker.labs.user_id = telegram_data.tg_users.user_id WHERE `bio_attacker`.`labs`.`user_id` = {self.user_id} LIMIT 1;")
-        if len(result) == 1: 
+        if len(result) != 0: 
             result[0].pop("patogen_names")
             result[0].pop("iris_name")
             result[0].pop("tg_users.id")
             result[0].pop("tg_users.user_id")
             self.__dict__ = dict(result[0])
             self.__start_data = dict(result[0])
-        else: return None
+        else: return False
 
     def __getitem__(self, item):
         return self.__dict__[item]
