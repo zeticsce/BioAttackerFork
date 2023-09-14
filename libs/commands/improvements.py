db75a278f7929a1221bf25e754efd3f31831f928
OPOLO <misha90638@gmail.com>
2023-09-14 18:00:41 +0300

    Update improvements.py

--- libs/commands/improvements.py
+++ libs/commands/improvements.py
@@ -4,31 +4,17 @@
 
 '''
 
-
-
 import os
-import shutil
-import asyncio
-import requests
-import random
-import subprocess
-import sys
-import datetime
 import re
-import time
-import math
 
 from app import dp, bot, query, strconv, save_message, is_host
 from config import MYSQL_HOST
 from Labs import Labs
-import calculate
 
-from aiogram import Bot, types
+from aiogram import types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 
-from math import ceil, floor
+from math import floor
 
 work_path = os.path.abspath(os.curdir)
 labs = Labs()
@@ -48,42 +34,103 @@ async def improve(message: types.Message):
         if lab.has_lab: # проверка существует ли лаба, если лабы нет, то пусть вообще не отвечает
         
             imps={}
-            imps["патоген"] =      re.fullmatch(r"\+(патоген|пат)(\s\d+)?", message.text.lower())
-            imps["квалификация"] = re.fullmatch(r"\+(разработка|квала|квалификация)(\s\d+)?", message.text.lower())
-            imps["заразность"] =   re.fullmatch(r"\+(зз|заразность)(\s\d+)?", message.text.lower())
-            imps["иммунитет"] =    re.fullmatch(r"\+(иммун|иммунитет)(\s\d+)?", message.text.lower())
-            imps["летальность"] =  re.fullmatch(r"\+(летал|летальность)(\s\d+)?", message.text.lower())
-            imps["безопасность"] = re.fullmatch(r"\+(сб|безопасность)(\s\d+)?", message.text.lower())
-            imps["попыток"] =      re.fullmatch(r"\+([а-я]+)(\s\d+)?", message.text.lower())
+            imps["патоген"] =      re.fullmatch(r"(\+\+|\+)(патоген|пат)(\s\d+)?", message.text.lower())
+            imps["квалификация"] = re.fullmatch(r"(\+\+|\+)(разработка|квала|квалификация)(\s\d+)?", message.text.lower())
+            imps["заразность"] =   re.fullmatch(r"(\+\+|\+)(зз|заразность)(\s\d+)?", message.text.lower())
+            imps["иммунитет"] =    re.fullmatch(r"(\+\+|\+)(иммун|иммунитет)(\s\d+)?", message.text.lower())
+            imps["летальность"] =  re.fullmatch(r"(\+\+|\+)(летал|летальность)(\s\d+)?", message.text.lower())
+            imps["безопасность"] = re.fullmatch(r"(\+\+|\+)(сб|безопасность)(\s\d+)?", message.text.lower())
+            imps["попыток"] =      re.fullmatch(r"(\+\+|\+)([а-я]+)(\s\d+)?", message.text.lower())
 
-            atts = int(imps["попыток"].group(2)) if imps["попыток"].group(2) != None else 1
+            atts = int(imps["попыток"].group(3)) if imps["попыток"].group(3) != None else 1
             text = None
 
             if atts <= 100:
+                if imps["попыток"].group(1) == "+": # тут проверяется колво плюсов, может быть только один или два
 
-                if imps["патоген"] != None:
-                    price = strconv.format_nums(impr_price(lab.all_patogens, lab.all_patogens + atts, 2.0))
-                    text = f"Прокачка патоегна с _{lab.all_patogens} ур._ до _{lab.all_patogens + atts} ур._ обойдется вам в _{price} био_"
+                    if imps["патоген"] != None:
+                        price = strconv.format_nums(impr_price(lab.all_patogens, lab.all_patogens + atts, 2.0))
+                        text = f"Прокачка патоегна с _{lab.all_patogens} ур._ до _{lab.all_patogens + atts} ур._ обойдется вам в _{price} био_"
 
-                elif imps["квалификация"] != None:
-                    price = strconv.format_nums(impr_price(lab.qualification, lab.qualification + atts, 2.0))
-                    text = f"Прокачка квалификации с _{lab.qualification} ур._ до _{lab.qualification + atts} ур._ обойдется вам в _{price} био_"
+                    elif imps["квалификация"] != None:
+                        price = strconv.format_nums(impr_price(lab.qualification, lab.qualification + atts, 2.0))
+                        text = f"Прокачка квалификации с _{lab.qualification} ур._ до _{lab.qualification + atts} ур._ обойдется вам в _{price} био_"
+                        if lab.qualification < 60:
+                            atts = atts if lab.qualification + atts <= 60 else 60 - lab.qualification
+                            price = impr_price(lab.qualification, lab.qualification + atts, 2.0)
+                            text = f"Прокачка квалификации с _{lab.qualification} ур._ до _{lab.qualification + atts} ур._ обойдется вам в _{price} био_"
+                        else: text = f"У вас уже максимальный уровень!"
 
-                elif imps["заразность"] != None:
-                    price = strconv.format_nums(impr_price(lab.infectiousness, lab.infectiousness + atts, 2.0))
-                    text = f"Прокачка заразности с _{lab.infectiousness} ур._ до _{lab.infectiousness + atts} ур._ обойдется вам в _{price} био_"
+                    elif imps["заразность"] != None:
+                        price = strconv.format_nums(impr_price(lab.infectiousness, lab.infectiousness + atts, 2.0))
+                        text = f"Прокачка заразности с _{lab.infectiousness} ур._ до _{lab.infectiousness + atts} ур._ обойдется вам в _{price} био_"
 
-                elif imps["иммунитет"] != None:
-                    price = strconv.format_nums(impr_price(lab.immunity, lab.immunity + atts, 2.0))
-                    text = f"Прокачка иммунитета с _{lab.immunity} ур._ до _{lab.immunity + atts} ур._ обойдется вам в _{price} био_"
+                    elif imps["иммунитет"] != None:
+                        price = strconv.format_nums(impr_price(lab.immunity, lab.immunity + atts, 2.0))
+                        text = f"Прокачка иммунитета с _{lab.immunity} ур._ до _{lab.immunity + atts} ур._ обойдется вам в _{price} био_"
 
-                elif imps["летальность"] != None:
-                    price = strconv.format_nums(impr_price(lab.mortality, lab.mortality + atts, 2.0))
-                    text = f"Прокачка летальности с _{lab.mortality} ур._ до _{lab.mortality + atts} ур._ обойдется вам в _{price} био_"
+                    elif imps["летальность"] != None:
+                        price = strconv.format_nums(impr_price(lab.mortality, lab.mortality + atts, 2.0))
+                        text = f"Прокачка летальности с _{lab.mortality} ур._ до _{lab.mortality + atts} ур._ обойдется вам в _{price} био_"
 
-                elif imps["безопасность"] != None:
-                    price = strconv.format_nums(impr_price(lab.security, lab.security + atts, 2.0))
-                    text = f"Прокачка безопасности с _{lab.security} ур._ до _{lab.security + atts} ур._ обойдется вам в _{price} био_"
+                    elif imps["безопасность"] != None:
+                        price = strconv.format_nums(impr_price(lab.security, lab.security + atts, 2.0))
+                        text = f"Прокачка безопасности с _{lab.security} ур._ до _{lab.security + atts} ур._ обойдется вам в _{price} био_"
+                        
+                else:
+
+                    if imps["патоген"] != None:
+                        price = impr_price(lab.all_patogens, lab.all_patogens + atts, 2.0)
+                        text = f"Вы успешно увелчили максимальное колличество патоегенов с _{lab.all_patogens}_ до _{lab.all_patogens + atts}_, это обошлось вам в _{strconv.format_nums(price)} био_"
+                        if lab.bio_res > price:
+                            lab.all_patogens += atts
+                            lab.bio_res -= price
+                        else: text = f"Недостаточно био-ресурса!"
+
+                    elif imps["квалификация"] != None:
+                        if lab.qualification < 60:
+                            atts = atts if lab.qualification + atts <= 60 else 60 - lab.qualification
+                            price = impr_price(lab.qualification, lab.qualification + atts, 2.0)
+                            text = f"Вы успешно прокачали квалификацию с _{lab.qualification} ур._ до _{lab.qualification + atts} ур._, это обошлось вам в _{strconv.format_nums(price)} био_"
+                            if lab.bio_res > price:
+                                lab.qualification += atts
+                                lab.bio_res -= price
+                            else: text = f"Недостаточно био-ресурса!"
+                        else: text = f"У вас уже максимальный уровень!"
+
+                    elif imps["заразность"] != None:
+                        price = impr_price(lab.infectiousness, lab.infectiousness + atts, 2.0)
+                        text = f"Вы успешно прокачали заразность с _{lab.infectiousness} ур._ до _{lab.infectiousness + atts} ур._, это обошлось вам в _{strconv.format_nums(price)} био_"
+                        if lab.bio_res > price:
+                            lab.infectiousness += atts
+                            lab.bio_res -= price
+                        else: text = f"Недостаточно био-ресурса!"
+
+                    elif imps["иммунитет"] != None:
+                        price = impr_price(lab.immunity, lab.immunity + atts, 2.0)
+                        text = f"Вы успешно прокачали иммунитет с _{lab.immunity} ур._ до _{lab.immunity + atts} ур._, это обошлось вам в _{strconv.format_nums(price)} био_"
+                        if lab.bio_res > price:
+                            lab.immunity += atts
+                            lab.bio_res -= price
+                        else: text = f"Недостаточно био-ресурса!"
+
+                    elif imps["летальность"] != None:
+                        price = impr_price(lab.mortality, lab.mortality + atts, 2.0)
+                        text = f"Вы успешно прокачали летальность с _{lab.mortality} ур._ до _{lab.mortality + atts} ур._, это обошлось вам в _{strconv.format_nums(price)} био_"
+                        if lab.bio_res > price:
+                            lab.mortality += atts
+                            lab.bio_res -= price
+                        else: text = f"Недостаточно био-ресурса!"
+
+                    elif imps["безопасность"] != None:
+                        price = impr_price(lab.security, lab.security + atts, 2.0)
+                        text = f"Вы успешно прокачали безопасность с _{lab.security} ур._ до _{lab.security + atts} ур._, это обошлось вам в _{strconv.format_nums(price)} био_"
+                        if lab.bio_res > price:
+                            lab.security += atts
+                            lab.bio_res -= price
+                        else: text = f"Недостаточно био-ресурса!"
+                    
+                    lab.save()
 
             else: text = f"Вы не можете прокачать более _100 уровней_ за раз!"
 
