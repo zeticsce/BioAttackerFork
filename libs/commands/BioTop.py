8f2b954b8b1ea1d6b789c25186f2247c24fbb640
OPOLO <misha90638@gmail.com>
2023-10-10 21:33:06 +0300

    Update BioTop.py

--- libs/commands/BioTop.py
+++ libs/commands/BioTop.py
@@ -23,7 +23,7 @@ async def improve(message: types.Message):
         all_bio_exp = query("SELECT SUM(bio_exp) as bio FROM `bio_attacker`.`labs`")[0]['bio']
         for lab in labs.bio_top:
             count += 1
-            text += f"\n{count}. [{strconv.deEmojify(lab['name'])}](tg://openmessage?user_id={lab['user_id']}) | {strconv.num_to_str(lab['bio_exp'])} опыта"
+            text += f'\n{count}. <a href="tg://openmessage?user_id={lab["user_id"]}">{strconv.deEmojify(lab["name"])}</a> | {strconv.num_to_str(lab["bio_exp"])} опыта'
 
         text += f"\n\nБанк био-опыта в игре: {strconv.num_to_str(all_bio_exp)}"
-        await bot.send_message(message.chat.id, text=text, parse_mode="Markdown")
+        await bot.send_message(message.chat.id, text=text, parse_mode="HTML")
