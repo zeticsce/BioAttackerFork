6a8f79bdf4b949d9ea42cfbcc5841a53fe9648d1
OPOLO <misha90638@gmail.com>
2023-11-15 19:34:21 +0300

    Update handlers.py

--- libs/handlers.py
+++ libs/handlers.py
@@ -229,7 +229,8 @@ async def handler(message: types.message):
     if message.text.lower() in ("биоферма", "биофарма", "биофа", "майн") and not message.forward_from:
 
         lab = labs.get_lab(message['from']['id'])
-        if lab.has_lab and lab.last_farma + (60*60*4) > int(time.time()):
+        if not lab.has_lab: return
+        if lab.last_farma + (60*60*4) > int(time.time()):
             minuts = 60*4 - int((int(time.time()) - lab.last_farma)/60)
             if minuts <= 20:
                 if minuts == 1: declination = "минута"
